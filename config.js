509af62e445c24bcf2a470bb7370d1dfd2a8398a
FizzGemge <108047082+FizzGemge@users.noreply.github.com>
2022-10-18 16:53:52 +0700

    Update config.js

--- config.js
+++ config.js
@@ -49,32 +49,33 @@ let wibh = moment.tz('Asia/Jakarta').format('HH')
 
 // kalo ga punya ketik "-" atau biarin aja biar ada creditsnya :v
 
-global.sig = 'https://instagram.com/_ctzhid' // ig
-global.sgh = 'https://github.com/WH-MODS-BOT/' // github
-global.sgc = 'https://chat.whatsapp.com/FQkbyTEfvES3D363uxMEFOI' // group whatsapp
-global.sdc = 'https://www.discord.com'//discord
-global.snh = 'https://www.youtube.com/channel/UCMx4e8anOq_Olt2nMSv0Cow' // my youtube channel
-global.sfb = 'https://www.facebook.com/' // facebook
-global.syt = 'https://www.youtube.com/' // my youtube channel "kalau ada yt ke 2 tarok link nya disini"
+global.sig = 'h
+https://instagram.com/fizz_135?igshid=MDE2OWE1N2Q=' // ig
+global.sgh = '-' // github
+global.sgc = 'https://chat.whatsapp.com/B6ol9DdGElJHUogjtYB02D' // group whatsapp
+global.sdc = '-'//discord
+global.snh = 'https://youtube.com/channel/UCmaMke2niQa8B_K7_pbAXbg' // my youtube channel
+global.sfb = '-' // facebook
+global.syt = '-' // my youtube channel "kalau ada yt ke 2 tarok link nya disini"
 
 /*============== PAYMENT ==============*/
-global.pdana = '08XXXXXXXXXX' //dana
-global.povo = '08XXXXXXXXXX' //ovo
-global.pgopay = '08XXXXXXXXXX' //gopay
-global.plinkaja = '08XXXXXXXXXX' //link aja
-global.ppulsa = '08XXXXXXXXXX' //telkomsel
-global.ppulsa2 = '08XXXXXXXXXX' // kalau ada kartu lain isi aja
-global.psaweria = 'https://saweria.com/mrw4h1d'
+global.pdana = '088294951448' //dana
+global.povo = '088294951448' //ovo
+global.pgopay = '088294951448' //gopay
+global.plinkaja = '-' //link aja
+global.ppulsa = '08294951448' //smartfren
+global.ppulsa2 = '-' // kalau ada kartu lain isi aja
+global.psociabuzz = 'https://sociabuzz.com/fizz_/tribe'
 
 /*============== NOMOR ==============*/
-global.nomorbot = '62XXXXXXXXXXX'
-global.nomorown = '6282127487538'
-global.namebot = 'WH-MODS-BOT-V1'
-global.nameown = 'WH MODS DEV'
+global.nomorbot = '6288294951448'
+global.nomorown = '6288296172396'
+global.namebot = '𝑯𝒊𝒏𝒂𝒕𝒂 𝑩𝒐𝒕'
+global.nameown = '𝑭𝒊𝒛𝒛𝒙'
 
 /*============== Github Thanks To ==============*/
-global.namagithub = 'Nama Github Belum Diisi' // nama github lu
-global.linkgithub = 'Link Githu Belum Diisi' // tarok link github lu
+global.namagithub = 'Fizzgemge' // nama github lu
+global.linkgithub = '-' // tarok link github lu
 
 // klau kgk ada github kasih tanda '-' aja biar kgk error
 
@@ -85,11 +86,11 @@ global.lolkey = 'e54205a4ca2caa368cc067bb' // PUNYA WAHID  8e66d0934cf741bfd2182
 
 /*============== STAFF ==============*/
 global.owner = [
-  ['6282127487538', 'WH MODS DEV', true],
-  ['-', 'ISI KALAU ADA', true],
-  ['-', 'ISI KALAU ADA', true],
-  ['-', 'ISI KALAU ADA', true],
-  ['-', 'ISI KALAU ADA', true]
+  ['6288296172396', '𝑭𝒊𝒛𝒛𝒙', true],
+  ['-', '-', true],
+  ['-', '-', true],
+  ['-', '-', true],
+  ['-', '-', true]
     //Ganti jd nomormu sama Namamu
   // [number, dia creator/owner?, dia developer?]
 ] // Put your number here
@@ -138,14 +139,14 @@ global.APIKeys = { // APIKey Here
 // Kata APIKEY itu isi dengan apikey lu sendiri, dengan beli ke website itu
 
 /*============== WATERMARK ==============*/
-global.wm = '                「 WH-MODS-BOT-V1 」' //Main Watermark
-global.wm2 = '꒷︶꒷꒥꒷ ‧₊˚ ꒰ฅ˘WH-MODS-BOT-V1 ˘ฅ ꒱ ‧₊˚꒷︶꒷꒥꒷'
-global.wm3 = '⫹⫺ WH-MODS-BOT-V1 '
+global.wm = '                「 ©𝑯𝒊𝒏𝒂𝒕𝒂 𝑩𝒐𝒕 」' //Main Watermark
+global.wm2 = '꒷︶꒷꒥꒷ ‧₊˚ ꒰ฅ˘𝑯𝒊𝒏𝒂𝒕𝒂 𝑩𝒐𝒕 ˘ฅ ꒱ ‧₊˚꒷︶꒷꒥꒷'
+global.wm3 = '⫹⫺ 𝑯𝒊𝒏𝒂𝒕𝒂 𝑩𝒐𝒕 '
 global.botdate = `⫹⫺ DATE: ${week} ${date}\n⫹⫺ 𝗧𝗶𝗺𝗲: ${wktuwib}`
 global.bottime = `T I M E : ${wktuwib}`
-global.titlebot = `⫹⫺ WhatsApp Bot | By WH-MODS-DEV`
+global.titlebot = `⫹⫺ WhatsApp Bot | By 𝑭𝒊𝒛𝒛𝒙`
 global.packname = '☂︎ ᴄʀᴇᴀᴛᴇᴅ ʙʏ'
-global.author = '                「 WH-MODS-BOT-V1  あ」'
+global.author = '                「 𝑯𝒊𝒏𝒂𝒕𝒂 𝑩𝒐𝒕  あ」'
 
 /*Fake*/
 global.fsizedoc = '1'.repeat(10)
